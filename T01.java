422302e220585c21b471023b517af8e4a9f1dd13
Boy Simamora <boyharendy321@gmail.com>
2024-09-06 10:44:04 +0700

    T01. Sudah Selesai mengerjakan Tugas

--- T01.java
+++ T01.java
@@ -1,1 +1,43 @@
-// NIM - Your Name
+// 12S24016 - Boy Harendy Simamora
+// 12S24055 - Theresia Samosir
+
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN;
+
+        iSBN = input.nextLine();
+        String jUDUL;
+
+        jUDUL = input.nextLine();
+        String pENULIS;
+
+        pENULIS = input.nextLine();
+        int tAHUNTERBIT;
+
+        tAHUNTERBIT = input.nextInt();
+        String pENERBIT;
+
+        pENERBIT = input.nextLine();
+        String fORMATBUKU;
+
+        fORMATBUKU = input.nextLine();
+        double hARGABELI;
+
+        hARGABELI = input.nextDouble();
+        double mINIMUMMARGIN;
+
+        mINIMUMMARGIN = input.nextDouble();
+        int sTOK;
+
+        sTOK = input.nextInt();
+        double rATING;
+
+        rATING = input.nextDouble();
+        System.out.println(iSBN + "|" + jUDUL + "|" + pENULIS + "|" + tAHUNTERBIT + "|" + pENERBIT + "|" + fORMATBUKU + "|" + hARGABELI + "|" + mINIMUMMARGIN + "|" + sTOK + "|" + rATING);
+    }
+}
